f349da45cc833568905668415496e007b3189fdf
vandat3012 <luongvandat301220@gmail.com>
2023-11-30 15:50:36 +0700

    update bai tap

--- javascript/colorGuessGame.js
+++ javascript/colorGuessGame.js
@@ -1,9 +1,9 @@
 let numberCircle = 9;
-let colors = [];
+let colors;
 let pickedColor;
 let circle = document.querySelectorAll(".circle");
 let messageDisplay = document.getElementById("message");
-
+let numberPick;
 function doColor() {
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
@@ -25,14 +25,21 @@ function changeColors(color) {
 function createCircle() {
     for (let i = 0; i < numberCircle; i++) {
         circle[i].addEventListener("click", function() {
-            let clickedColor = this.style.backgroundColor;
-            if(clickedColor === pickedColor) {
-                messageDisplay.textContent = "Correct";
-                changeColors(pickedColor);
-            }
-            else {
-                this.style.backgroundColor = "rgba(255,255,255,0.91)";
-                messageDisplay.textContent = "try again";
+            if (numberPick > 0) {
+                let clickedColor = this.style.backgroundColor;
+                if(clickedColor === pickedColor) {
+                    messageDisplay.textContent = "Correct!!!!!!!!! You loaded it for the first time, right?" ;
+                    changeColors(pickedColor);
+                }
+                else {
+                    this.style.backgroundColor = "rgba(255,255,255,0.91)";
+                    numberPick--;
+                    if (numberPick === 0) {
+                        messageDisplay.textContent = " Do you want to wash your hands again? "
+                    } else {
+                        messageDisplay.textContent = "Try again! " + " You only have " + numberPick + " turns to choose" ;
+                    }
+                }
             }
         });
     }
@@ -57,6 +64,31 @@ function reset() {
 }
 reset();
 
+function setDifficulty(difficulty) {
+    if (difficulty === "easy") {
+        numberPick = 6;
+    } else if (difficulty === "hard") {
+        numberPick = 4;
+    }else if (difficulty === "master"){
+        numberPick = 2;
+    }
+    reset();
+}
+document.getElementById("easyButton").addEventListener("click", function() {
+    setDifficulty("easy");
+});
+
+document.getElementById("hardButton").addEventListener("click", function() {
+    setDifficulty("hard");
+});
+
+document.getElementById("masterButton").addEventListener("click", function() {
+    setDifficulty("master");
+});
+
+setDifficulty("easy");
+
 function replay(){
     window.location.reload();
 }
+
